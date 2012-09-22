02487f1bf73e7f328dcfda1f791b37f9ca786d81
Vinicius <Vinicius@Vinicius-PC>
2012-09-22 17:09:05 -0300

    versão lexica

---
+++ src/gals/Constants.java
@@ -0,0 +1,55 @@
+package gals;
+
+public interface Constants extends ScannerConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_palavraReservada = 2;
+    int t_identificador = 3;
+    int t_constNum = 4;
+    int t_constLit = 5;
+    int t_and = 6;
+    int t_array = 7;
+    int t_character = 8;
+    int t_do = 9;
+    int t_endDo = 10;
+    int t_exit = 11;
+    int t_false = 12;
+    int t_if = 13;
+    int t_ifFalseDo = 14;
+    int t_ifTrueDo = 15;
+    int t_logical = 16;
+    int t_not = 17;
+    int t_number = 18;
+    int t_or = 19;
+    int t_read = 20;
+    int t_round = 21;
+    int t_true = 22;
+    int t_trunk = 23;
+    int t_write = 24;
+    int t_TOKEN_25 = 25; //"|"
+    int t_TOKEN_26 = 26; //":"
+    int t_TOKEN_27 = 27; //"."
+    int t_TOKEN_28 = 28; //"]"
+    int t_TOKEN_29 = 29; //"["
+    int t_TOKEN_30 = 30; //","
+    int t_TOKEN_31 = 31; //":="
+    int t_TOKEN_32 = 32; //"("
+    int t_TOKEN_33 = 33; //")"
+    int t_TOKEN_34 = 34; //"{"
+    int t_TOKEN_35 = 35; //"}"
+    int t_TOKEN_36 = 36; //"+"
+    int t_TOKEN_37 = 37; //"-"
+    int t_TOKEN_38 = 38; //"*"
+    int t_TOKEN_39 = 39; //"/"
+    int t_TOKEN_40 = 40; //"\"
+    int t_TOKEN_41 = 41; //"%"
+    int t_TOKEN_42 = 42; //"="
+    int t_TOKEN_43 = 43; //"!="
+    int t_TOKEN_44 = 44; //"<"
+    int t_TOKEN_45 = 45; //"<="
+    int t_TOKEN_46 = 46; //">"
+    int t_TOKEN_47 = 47; //">="
+
+}
---
+++ src/models/Compilador.java
@@ -0,0 +1,19 @@
+package models;
+
+import gals.Lexico;
+
+/**
+ * @author Vinicius Ferneda de Lima
+ */
+public class Compilador {
+
+    private Lexico lexico;
+    
+    public Compilador(){
+        this.lexico = new Lexico();
+    }
+    
+    public String compilar(String codigo){
+        return null;
+    }
+}
